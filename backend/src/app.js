c1ee3e4b4a7f34bee51cd19d56f7035cebdc3395
Adam Ahmad <adamahmad8890@gmail.com>
2026-06-06 22:53:28 +0300

    I Update The frontend and added Photos, Admin dashboard, gallery, news, and registrations pages

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,9 @@ const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 connectDB();
-
+// { origin: process.env.CLIENT_URL, credentials: true }
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan('dev'));
 app.use('/api', rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
