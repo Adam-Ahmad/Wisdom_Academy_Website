c5376f1da82e8594a09c7c4b9e15fb0984a135a7
Adam Ahmad <adamahmad8890@gmail.com>
2026-06-07 21:52:06 +0300

    Added Verser API

--- frontend/admin/js/admin.js
+++ frontend/admin/js/admin.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = process.env.API_BASE || "https://wisdom-academy-website-backend.vercel.app/api";
 
 // Auth Guard
 function requireAdmin() {
