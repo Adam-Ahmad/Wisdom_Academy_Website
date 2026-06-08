8e7406e74c4a624c0278a25778ae2c132135afdf
Adam Ahmad <adamahmad8890@gmail.com>
2026-06-08 19:40:15 +0300

    I Added Health Check Route

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
 const app = require('./src/app');
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Backend is running successfully!" });
+});
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+
