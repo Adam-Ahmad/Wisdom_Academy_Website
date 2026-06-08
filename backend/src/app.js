b15b05b883f77c9dce482835a47a65ef138886f8
Adam Ahmad <adamahmad8890@gmail.com>
2026-06-08 20:35:14 +0300

    Add CORS Options

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,21 @@ const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 connectDB();
-// { origin: process.env.CLIENT_URL, credentials: true }
+
+
 app.use(helmet());
 app.use(cors());
+
+// Configure CORS options
+const corsOptions = {
+  origin: 'https://wisdom-academy-website.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
+
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan('dev'));
 app.use('/api', rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
